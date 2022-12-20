ad22db1d90d9fb16509c6c7ccbca61390521152e
Manuel Grgic Figueiredo <44705459+ManuGr@users.noreply.github.com>
2022-12-20 11:32:58 +0100

    Add Home section and navbar

---
+++ js/script.js
@@ -0,0 +1,10 @@
+import Typed from 'typed.js';
+
+var options = {
+    strings: ['Fullstack Web Developer', 'Freelancer'],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+}
+
+var typed = new Typed('.auto-type', options);
